0b3cba341a0f8382b96a279ae10d7bf1a637e0c1
ahmad <ahmad_almujawer@icloud.com>
2024-01-22 11:48:32 +0100

    Update sql tables and insert some dummy data

--- server/setup.sql
+++ server/setup.sql
@@ -1,17 +1,26 @@
+DROP DATABASE IF EXISTS home_office;
+CREATE DATABASE home_office;
+USE home_office;
+
 CREATE TABLE users (
-     id SERIAL PRIMARY KEY,
-     first           VARCHAR NOT NULL CHECK (first != ''),
-     last            VARCHAR NOT NULL CHECK (last != ''),
-     email           VARCHAR NOT NULL UNIQUE CHECK (email != ''),
-     hashed_password VARCHAR NOT NULL CHECK (hashed_password != ''),
+     id INT PRIMARY KEY AUTO_INCREMENT,
+     first           VARCHAR(50) NOT NULL CHECK (first != ''),
+     last            VARCHAR(50) NOT NULL CHECK (last != ''),
+     email           VARCHAR(50) UNIQUE CHECK (email IS NULL OR email != ''), -- Email ist  optional
+     hashed_password VARCHAR(50) NOT NULL CHECK (hashed_password != ''),
      created         TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     
     );
 
-CREATE TABLE HomeOfficeTime (
-    id SERIAL PRIMARY KEY,
+
+CREATE TABLE home_office_time (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     userID    INT REFERENCES users(id),
     startTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    endTime   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    endTime   TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- CHECK (endTime >= startTime)
     date      TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO users (first, last, email, hashed_password, created)
+VALUES ("Ahmad", "MJ", NULL , "1234",CURRENT_TIMESTAMP),
+       ("Joe", "Hafry", "joe@test@de", "12345",CURRENT_TIMESTAMP);
