69f35daa1aee8cb0fe07792e52c8a4e3751a1fb0
ahmad <ahmad_almujawer@icloud.com>
2024-01-23 11:14:54 +0100

    Update bcrypt function and catch errors

--- server/bc.js
+++ server/bc.js
@@ -1,9 +1,20 @@
 const bcrypt = require("bcryptjs");
 
-exports.hash = (password) => {
-    return bcrypt.genSalt().then((salt) => {
-        return bcrypt.hash(password, salt);
-    });
+exports.hash = async (password) => {
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+  } catch (error) {
+    throw new Error(`Error hashing password: ${error.message}`);
+  }
 };
 
-exports.compare = bcrypt.compare;
+exports.compare = async (password, hashedPassword) => {
+  try {
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+    return isMatch;
+  } catch (error) {
+    throw new Error(`Error comparing passwords: ${error.message}`);
+  }
+};
