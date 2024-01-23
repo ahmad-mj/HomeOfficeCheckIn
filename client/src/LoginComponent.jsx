04924518edc5b11e088c190ae0421ac8515d17af
ahmad <ahmad_almujawer@icloud.com>
2024-01-23 09:45:49 +0100

    Add login component and styling

---
+++ client/src/LoginComponent.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function LoginComponent() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const userId = 'ahmad@test@de'
+      const plainPassword = "1234"
+      const response = await axios.post("http://localhost:3000/login", {
+        email: userId,
+        password: plainPassword,
+      });
+      console.log('Response from server:', response.data);
+      if (response.data.message === "Login success") {
+        setError("");
+        alert("Login successful!");
+       // redirect to homeoffice start? 
+      } else {
+        setError("Invalid username or password");
+      }
+    } catch (error) {
+      console.error("Login error:", error.message);
+      setError("Internal Server Error");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="login">
+      <header>Login</header>
+      <div className="login-form">
+        <h3>HomeOffice CheckIn - Login</h3>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label>Username:</label>
+            <input
+              type="text"
+              name="username"
+              value={username}
+              onChange={handleUsernameChange}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label>Password:</label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={handlePasswordChange}
+              required
+            />
+          </div>
+          <div className="input-group btn">
+            <button type="submit" disabled={loading}>
+              {loading ? "Logging in..." : "Login"}
+            </button>
+          </div>
+          {error && <div className="error-message">{error}</div>}
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default LoginComponent;
