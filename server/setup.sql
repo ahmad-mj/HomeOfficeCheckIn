2b4477dd89e89229d79fdf42dfa097f11b7cfec5
ahmad <ahmad_almujawer@icloud.com>
2024-01-23 11:15:19 +0100

    Update database as required

--- server/setup.sql
+++ server/setup.sql
@@ -4,9 +4,7 @@ USE home_office;
 
 CREATE TABLE users (
      id INT PRIMARY KEY AUTO_INCREMENT,
-     first           VARCHAR(50) NOT NULL CHECK (first != ''),
-     last            VARCHAR(50) NOT NULL CHECK (last != ''),
-     email           VARCHAR(50) UNIQUE CHECK (email IS NULL OR email != ''), -- Email ist  optional
+     user_name           VARCHAR(50) NOT NULL CHECK (user_name != ''),
      hashed_password VARCHAR(50) NOT NULL CHECK (hashed_password != ''),
      created         TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     
@@ -14,13 +12,13 @@ CREATE TABLE users (
 
 
 CREATE TABLE home_office_time (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    userID    INT REFERENCES users(id),
-    startTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    endTime   TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- CHECK (endTime >= startTime)
-    date      TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+   id INT PRIMARY KEY AUTO_INCREMENT,
+   userID INT REFERENCES users(id),
+   timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO users (first, last, email, hashed_password, created)
-VALUES ("Ahmad", "MJ", NULL , "1234",CURRENT_TIMESTAMP),
-       ("Joe", "Hafry", "joe@test@de", "12345",CURRENT_TIMESTAMP);
+
+INSERT INTO users (user_name, hashed_password, created)
+VALUES ("Ahmad MJ" , "ahmad",CURRENT_TIMESTAMP),
+       ("Noha MS" , "noha" ,CURRENT_TIMESTAMP),
+       ("Joe Hafry" , "joe" ,CURRENT_TIMESTAMP);
